4e26fc447e2fdef27775c2dfad8e1a684a4f1506
Shaswat Dubey <shaswat185dubey@gmail.com>
2024-06-22 17:20:05 +0530

    component

---
+++ src/World.jsx
@@ -0,0 +1,12 @@
+function World() {
+ return(
+    <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat soluta et
+    labore explicabo odit quos laudantium minus assumenda sed! Nam, tenetur amet
+    officia odio accusamus fugiat in. Repellat, quas vel?
+  </p>
+
+ );
+}
+
+export default World;
